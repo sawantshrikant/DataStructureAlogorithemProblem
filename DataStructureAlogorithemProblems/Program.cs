badb9ea8b4bc95328baa641241391787108a75be
Shrikant <sawantshrikant07@gmail.com>
2023-03-30 14:09:27 +0530

    [Shrikant] add:WelcomeAlogorithemProblems

---
+++ DataStructureAlogorithemProblems/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace DataStructureAlgorithmProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structure Algorithm Problem!");
+
+
+
+        }
+    }
+}
